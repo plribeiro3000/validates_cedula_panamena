d0f2b2dafeffbf0ae0a4a47acfd00091b2160ff1
Paulo Ribeiro <plribeiro3000@gmail.com>
2023-03-20 21:07:36 -0300

    chore(*): Initial Commit

---
+++ lib/validates_cedula_panamena.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "validates_cedula_panamena/version"
+
+module ValidatesCedulaPanamena
+  class Error < StandardError; end
+  # Your code goes here...
+end
